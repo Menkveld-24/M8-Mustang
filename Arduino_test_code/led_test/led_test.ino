caf4fb77e3b526274a4dada4b38fc3bebba54c68
dbimaade <94497089+dbimaade@users.noreply.github.com>
2022-06-03 12:44:57 +0200

    Update led_test.ino

--- Arduino_test_code/led_test/led_test.ino
+++ Arduino_test_code/led_test/led_test.ino
@@ -3,33 +3,27 @@
 #define NUM_LEDS    10
 
 CRGBArray<NUM_LEDS> leds;
-byte color = 255;
+byte color = 100;
 byte dim = 155;
-byte saturation = 255;
+byte saturation = 200;
+
 void setup() {
 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
-//  leds[0] = CRGB::White;
-//  FastLED.show();
 }
 
 void loop() {
-  color--;
-  dim++;
-  saturation --;
-  for (int i = 0; i <= 3; i++) {
-    leds[i] = CHSV(color, saturation, dim);
+  for (int i = 0; i <= 9; i+= 2) {
+    leds[i] = CHSV(color+i*20, saturation, dim);
+    leds[i+1] = CHSV(color+i*20, saturation, dim);
     FastLED.show();
-    delay(200);
+    delay(50);
   }
-  for (int i = 4; i <=9; i++){
-    leds[i] = CHSV(color, saturation-50, dim);
+
+  for (int i = 9; i >= 0; i-= 2) {
+    leds[i] = CHSV(color-i*20, saturation, dim);
+    leds[i-1] = CHSV(color-i*20, saturation, dim);
     FastLED.show();
-    delay(200);
-  }
-  for (int i = 9; i >= 0; i--) {
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(200);
+    delay(50);
   }
 }
