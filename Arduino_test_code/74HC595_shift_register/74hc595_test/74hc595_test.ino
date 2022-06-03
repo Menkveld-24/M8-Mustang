4207b67894feef0a9d531dafb4d4c14d4ee87221
Menkveld <menke.veerman@gmail.com>
2022-06-03 11:37:31 +0200

    rotary_test

--- Arduino_test_code/74HC595_shift_register/74hc595_test/74hc595_test.ino
+++ Arduino_test_code/74HC595_shift_register/74hc595_test/74hc595_test.ino
@@ -45,7 +45,12 @@ int dataPin = 11;
 
 //byte currentStep = 0; // 0-3 steps, index of STEPS
 bool toggle = false;
+#define outputA 6
+ #define outputB 7
 
+ int counter = 0; 
+ int aState;
+ int aLastState;  
 unsigned long step_delay = 0;
 
 void setup() {
@@ -55,11 +60,29 @@ void setup() {
   pinMode(dataPin, OUTPUT);
   pinMode(13, OUTPUT);
   step_delay = 60L * 1000L * 1000L / 1024 / 10;
+  pinMode (outputA,INPUT);
+   pinMode (outputB,INPUT);
+   
+   Serial.begin (9600);
+   // Reads the initial state of the outputA
+   aLastState = digitalRead(outputA);  
 }
 
 void loop() {
-  delay(1000);
-  stepm(300);
+  aState = digitalRead(outputA); // Reads the "current" state of the outputA
+     // If the previous and the current state of the outputA are different, that means a Pulse has occured
+     if (aState != aLastState){     
+       // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+       if (digitalRead(outputB) != aState) { 
+         counter ++;
+       } else {
+         counter --;
+       }
+       Serial.print("Position: ");
+       Serial.println(counter);
+     } 
+     aLastState = aState; // Updates the previous state of the outputA with the current state
+//  stepm(300);
 }
 int step_number = 0;
 
